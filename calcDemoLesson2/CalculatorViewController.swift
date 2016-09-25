52f5f782df4931a0b8792fcc98076c80ae20b009
Sergeeva Anastasia <neko@myaw.ru>
2016-09-25 20:15:13 +0300

    Constraints added

--- calcDemoLesson2/CalculatorViewController.swift
+++ calcDemoLesson2/CalculatorViewController.swift
@@ -5,7 +5,21 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+    let darkColour = UIColor(red:0.21, green:0.13, blue:0.23, alpha:1.00).cgColor
+    let dark2Colour =  UIColor(red:0.57, green:0.07, blue:0.27, alpha:1.00).cgColor
+    let mediumColour = UIColor(red:0.81, green:0.29, blue:0.19, alpha:1.00).cgColor
+    let lightColour = UIColor(red:0.93, green:0.55, blue:0.17, alpha:1.00).cgColor
+    let contrastColour = UIColor(red:0.53, green:0.66, blue:0.15, alpha:1.00).cgColor
 
-
+    
+//    override func viewDidLoad() {
+//        let gradientBG = CAGradientLayer()
+//        gradientBG.frame = self.view.bounds
+//        gradientBG.colors = [darkColour, dark2Colour, darkColour, dark2Colour, darkColour]
+//        gradientBG.startPoint = CGPoint(x: 0.0, y: 0.0)
+//        gradientBG.endPoint = CGPoint(x: 0.0, y: 1.0)
+//        self.view.layer.insertSublayer(gradientBG, at: 0)
+//    }
+    
 }
 
